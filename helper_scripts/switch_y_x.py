a3b83af2d24ca137b20583b1592c1fa7e83cf873
Amrita Goswami <amrita16thaug646@gmail.com>
2024-10-20 15:47:58 +0000

    res: Added some input files
    Co-authored-by: Moritz Sallermann <moritzsallermann@gmail.com>

--- helper_scripts/switch_y_x.py
+++ helper_scripts/switch_y_x.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 output_folder = Path('/home/amritagos/Git/Github/iceSurfaceWorkFlow/res/ice_surfaces_genIce')
 
+# Create if it doesn't exist
+Path(output_folder).mkdir(parents=True, exist_ok=True)
+
 infilepaths = list(Path('/home/amritagos/Git/Github/GenIce/').glob('*.xyz'))
 
 for infilepath in infilepaths:
@@ -17,6 +20,7 @@ for infilepath in infilepaths:
         atom.position[1] = new_y
         atom.position[2] = new_z
     # from the CIF file
-    atoms.set_cell([30.590228902373884, 26.502128397830692, 14.377611329389692])
+    atoms.set_cell([30.590228902373884, 26.502128397830692, 21.566416994084538])
+    atoms.set_pbc(True)
     write(outfilepath, atoms, format='extxyz')
 
