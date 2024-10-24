e78450234011e55d845808d9649f9e8cfae5bf3e
Amrita Goswami <amrita16thaug646@gmail.com>
2024-10-24 13:56:30 +0000

    relax_box: Script for relaxing the box
    Should write out metadata as well.

---
+++ 1-relax_analyze_surfaces/workflow/scripts/relax_box.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+import numpy as np
+from ase.io import read, write
+import sys
+import json
+
+sys.path.insert(1, "/hpctheochem/amrita/scmecpp/")
+from ase_interface import SCME_PS
+
+
+def main(
+    input_xyz: Path,
+    seed: int,
+    output_xyz: Path,
+    results_file: Path,
+):
+
+    # Read the system using ASE
+    system = read(input_xyz, format="extxyz")
+    system.set_pbc(True)
+
+    system.calc = SCME_PS(
+        system,
+        NC=np.array([0, 0, 0]),
+        numerical=False,
+        dms=True,
+        irigidmolecules=False,
+        system=np.ones(1) * 1,
+    )
+    """,
+                     te=te,
+                     td=td,
+                     Ar=Ar,
+                     Br=Br,
+                     Cr=Cr,
+                     rc_Disp=rc_Disp)"""
+
+    from ase.optimize import BFGS
+
+    dyn = BFGS(system)  # Don't write out a trajectory
+    dyn.run(fmax=0.01)
+
+    energy = system.get_potential_energy()
+    n_water_molecules = len(system) / 3
+    cohesive_energy = energy / n_water_molecules
+
+    system._calc.get_dipole_moment()  # Calculate the dipole moment
+    dipole_moment_magnitude = np.linalg.norm(system._calc.results["dipole"])
+
+    # Write out the final XYZ file
+    write(output_xyz, system, format="xyz")
+
+    # Write results file as json or something easily parseable
+    with open(results_file, "w") as f:
+        f.write(
+            json.dumps(
+                dict(
+                    seed=seed,
+                    cohesive_energy=cohesive_energy,
+                    dipole_moment_vector=system._calc.results["dipole"],
+                    dipole_moment=dipole_moment_magnitude,
+                ),
+                indent=4,
+            )
+        )
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("input_xyz", type=Path)
+    parser.add_argument("seed", type=int)
+    parser.add_argument("output_xyz", type=int)
+    parser.add_argument("results_file", type=Path)
+
+    args = parser.parse_args()
+
+    main(
+        args.input_xyz,
+        args.seed,
+        args.output_xyz,
+        args.results_file,
+    )
