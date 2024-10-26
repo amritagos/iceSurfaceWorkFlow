c8411d2b33d05337ef4398689182c1250befd5ef
Amrita Goswami <amrita16thaug646@gmail.com>
2024-10-26 15:31:09 +0000

    drop_h2o: Workflow handles putting h2O molecules
    Co-authored-by: Moritz Sallermann <moritzsallermann@gmail.com>

--- 2-dropH2O/workflow/scripts/aggregate.py
+++ 2-dropH2O/workflow/scripts/aggregate.py
@@ -1,50 +1,66 @@
 from pathlib import Path
 import sys
 import json
-from typing import List 
+from typing import List
 import pandas as pd
 
+
 def main(
-    results_path_a: List[Path],
-    results_path_b: List[Path],
-    output_path: Path,
+    result_paths: List[Path],
+    isolated_water_energy: float,
+    output_csv: Path,
 ):
 
     df = pd.DataFrame()
-    water_energy = 3.6474808763708796e-06
 
     results = []
 
-    print(results_path_b)
-
-    for p in results_path_a:
-        with open(p,"r") as f:
+    for p in result_paths:
+        with open(p, "r") as f:
             res = json.load(f)
-            relative_energy = res["energy"] - res["surface_energy"] - water_energy
-            results.append( [ relative_energy, "A"] )
-    
-    for p in results_path_b:
-        with open(p,"r") as f:
-            res = json.load(f)
-            relative_energy = res["energy"] - res["surface_energy"] - water_energy
-            results.append( [ relative_energy, "B"] )
+            site_type = res["site_type"]
+            seed = res["seed"]
+            site_index = res["site_index"]
+            surface_energy = res["surface_energy"]
+            system_energy = res["system_energy"]
+            dipole_moment = res["dipole_moment"]
+            binding_energy = system_energy - surface_energy - isolated_water_energy
+            results.append(
+                [
+                    site_type,
+                    seed,
+                    site_index,
+                    binding_energy,
+                    dipole_moment,
+                    system_energy,
+                    surface_energy,
+                ]
+            )
 
-    df = pd.DataFrame(results, columns=["relative_energy", "site_type"])
-    df.to_csv( output_path )
+    df = pd.DataFrame(
+        results,
+        columns=[
+            "site_type",
+            "seed",
+            "site_index",
+            "binding_energy",
+            "dipole_moment",
+            "system_energy",
+            "isolated_surface_energy",
+        ],
+    )
+    df.to_csv(output_csv)
+
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--results_a", type=Path, nargs="+")
-    parser.add_argument("--results_b", type=Path, nargs="+")
-    parser.add_argument("--output", type=Path)
+    parser.add_argument("--result_paths", type=Path, nargs="+")
+    parser.add_argument("--isolated_water_energy", type=float)
+    parser.add_argument("--output_csv", type=Path)
 
     args = parser.parse_args()
 
-    main(
-        args.results_a,
-        args.results_b,
-        args.output
-    )
+    main(args.result_paths, args.isolated_water_energy, args.output_csv)
--- 2-dropH2O/workflow/scripts/drop_h2o.py
+++ 2-dropH2O/workflow/scripts/drop_h2o.py
@@ -1,20 +1,95 @@
 from pathlib import Path
+from typing import List
+from ase import Atoms
 import numpy as np
-from ase.io import read
+from ase.io import read, write
 from ase.build import molecule
 import sys
 import json
 
-sys.path.insert(1, "/hpctheochem/amrita/scmecpp/")
+sys.path.insert(1, "/home/amritagos/Git/Gitlab/scmecpp/")
 from ase_interface import SCME_PS
+
+
+def shift_atom(
+    point: np.ndarray, reference_point: np.ndarray, box_dimensions: np.ndarray
+) -> np.ndarray:
+    """Shift point coordinates, so that it is an unwrapped coordinate with respect to the reference point
+
+    Args:
+        point (np.ndarray): Position to be shifted
+        reference_point (np.ndarray): Position of the reference point
+        box_dimensions (np.ndarray): box lengths
+
+    Returns:
+        shifted_point: Shifted coordinates of the point
+    """
+    delta = point - reference_point
+    for i in range(len(delta)):
+        delta[i] -= box_dimensions[i] * round(delta[i] / box_dimensions[i])
+
+    shifted_point = reference_point + delta
+    return shifted_point
+
+
+def get_dropped_water_coordinates(
+    site_indices: List[int], surface_system: Atoms, shifting_distance=2.0
+) -> np.ndarray:
+    """Get the coordinates of the position where a water molecule should be dropped, onto a site defined by site_indices. The water molecule is placed shifting_distance Angstrom above this.
+    WARNING: Assumes the box starts from 0,0,0
+
+    Args:
+        site_indices (List[int]): site indices that describe the site (a trio of O atoms)
+        surface_system (Atoms): Atoms object for the surface
+
+    Returns:
+        np.ndarray: coordinates where the water molecule should go
+    """
+    surface_system_pos = surface_system.get_positions()
+    pos0 = surface_system_pos[site_indices[0]]
+    pos1 = surface_system_pos[site_indices[1]]
+    pos2 = surface_system_pos[site_indices[2]]
+
+    # Smallest box dimension
+    box_lengths = surface_system.cell.cellpar()[:3]
+    smallest_box_length = np.min(box_lengths)
+
+    # Check distance between 0 and 1
+    if (
+        surface_system.get_distance(site_indices[0], site_indices[1], mic=False)
+        > 0.5 * smallest_box_length
+    ):
+        # Shift point 1 around, keeping 0 constant
+        pos1 = shift_atom(pos1, pos0, box_lengths)
+    # Check the distance between 0 and 2
+    if (
+        surface_system.get_distance(site_indices[0], site_indices[2], mic=False)
+        > 0.5 * smallest_box_length
+    ):
+        # Shift point 2 around, keeping 0 constant
+        pos2 = shift_atom(pos2, pos0, box_lengths)
+    # Get the average position :
+    water_site_pos = np.mean(np.array([pos0, pos1, pos2]), axis=0)
+    # Check that the new site position is inside the box
+    # WARNING: assuming box starts from 0,0,0 (also don't do this for the z coordinate)
+    for i in range(3):
+        if surface_system.pbc[i] == True:
+            water_site_pos[i] = water_site_pos[i] % box_lengths[i]
+    # water_site_pos = water_site_pos % box_lengths
+    # Shift the z coordinate by 2 Angstrom
+    water_site_pos = water_site_pos + np.array([0, 0, shifting_distance])
+    return water_site_pos
 
 
 def main(
     input_surface_file: Path,
     site_file: Path,
-    results_file: Path,
+    seed: int,
+    site_type: str,
     site_idx: int,
     surface_energy: float,
+    metadata_file: Path,
+    out_xyz: Path,
 ):
 
     # Read the surface using ASE
@@ -24,19 +99,9 @@ def main(
     all_sites = np.load(site_file)
     current_site_ind = all_sites[site_idx]
     # Drop the water molecule onto the site, 2 Angstrom above the averaged z coordinate of the trio (denoting the site)
-    surface_system_pos = surface_system.get_positions()
-    site_avg_coord = np.mean(
-        np.array(
-            [
-                surface_system_pos[current_site_ind[0]],
-                surface_system_pos[current_site_ind[1]],
-                surface_system_pos[current_site_ind[2]],
-            ]
-        ),
-        axis=0,
+    water_site = get_dropped_water_coordinates(
+        current_site_ind, surface_system, shifting_distance=2.0
     )
-    # the desired z coordinate should be 2 Angstrom above this
-    water_site = site_avg_coord + np.array([0.0, 0.0, 2.0])
     # Create the new water molecule
     single_water = molecule("H2O")
     displacement = (
@@ -46,7 +111,7 @@ def main(
     # Shift the water molecule to the desired position
     system = surface_system + single_water
     system.cell = surface_system.cell
-    system.set_pbc(True)
+    system.pbc = surface_system.pbc
 
     system.calc = SCME_PS(
         system,
@@ -67,18 +132,29 @@ def main(
     from ase.optimize import BFGS
 
     dyn = BFGS(system)
-    dyn.run(fmax=0.01)
+    dyn.run(fmax=0.01, steps=0)
+
+    # Write out the final XYZ file
+    write(out_xyz, system)
 
     # energy = 0
     energy = system.get_potential_energy()
+    system._calc.get_dipole_moment()  # Calculate the dipole moment
+    dipole_moment_magnitude = np.linalg.norm(system._calc.results["dipole"])
 
-    # TODO: write results file as json or something easily parseable
-    with open(results_file, "w") as f:
+    # Write results into metadata JSON file
+    with open(metadata_file, "w") as f:
         f.write(
-            json.dumps(dict(
-                energy = energy,
-                surface_energy = surface_energy,
-                site_index = site_idx), indent=4
+            json.dumps(
+                dict(
+                    seed=seed,
+                    site_type=site_type,
+                    site_index=site_idx,
+                    surface_energy=surface_energy,
+                    system_energy=energy,
+                    dipole_moment=dipole_moment_magnitude,
+                ),
+                indent=4,
             )
         )
 
@@ -88,18 +164,24 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("input_surface_path", type=Path)
-    parser.add_argument("site_path", type=Path)
-    parser.add_argument("results_file", type=Path)
-    parser.add_argument("site_index", type=int)
-    parser.add_argument("surface_energy", type=float)
+    parser.add_argument("--input_surface_path", type=Path)
+    parser.add_argument("--site_path", type=Path)
+    parser.add_argument("--seed", type=int)
+    parser.add_argument("--site_type", type=str)
+    parser.add_argument("--site_index", type=int)
+    parser.add_argument("--surface_energy", type=float)
+    parser.add_argument("--out_metadata", type=Path)
+    parser.add_argument("--out_xyz", type=Path)
 
     args = parser.parse_args()
 
     main(
         args.input_surface_path,
         args.site_path,
-        args.results_file,
+        args.seed,
+        args.site_type,
         args.site_index,
         args.surface_energy,
+        args.out_metadata,
+        args.out_xyz,
     )
