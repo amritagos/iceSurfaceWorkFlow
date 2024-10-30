c32678eca5271cab0a455fb0e3ba03131b5b731f
Amrita Goswami <amrita@hi.is>
2024-10-30 10:14:13 +0000

    bugfix: dropH2O

--- 2-dropH2O/workflow/scripts/drop_h2o.py
+++ 2-dropH2O/workflow/scripts/drop_h2o.py
@@ -215,7 +215,7 @@ def main(
 
     # Check the final site position, and site type
     site_moved, final_site_type = check_final_site_conditions(
-        dropped_o_index, site_type, current_site_ind, system, o_o_cutoff=3.5
+        dropped_o_index, site_type, current_site_ind, system
     )
 
     # Write results into metadata JSON file
